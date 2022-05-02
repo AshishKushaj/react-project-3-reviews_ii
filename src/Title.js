199bf5ea70f1b42e2661b56dfb34a5c6aebb4715
Ashish Kushaj <ashishkushajak@gmail.com>
2022-05-02 16:42:19 +0530

    changes

---
+++ src/Title.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function Title(){
+    return(
+        <div className="title ">
+            <h1>Reviews</h1>
+        </div>
+    );
+};
+
+
+export default Title;
