364db2417787542a2e1a57367bd3baf1f126f417
Ashish Kushaj <ashishkushajak@gmail.com>
2022-05-03 12:27:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import './App.css';
 import Title from './Title';
 import Box from './Box';
 import './data'; 
+import React from 'react';
+
+
 
 
 function App() {
 
-  
+  const {id , name , job , image , text} =data[0];
 
   return (
    <div >
